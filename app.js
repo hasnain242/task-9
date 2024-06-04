86d9703506b2d26519d73b6b7d14813a0010182a
hasnain242 <78303526+hasnain242@users.noreply.github.com>
2024-06-05 04:44:01 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var num1=5;
+var num2=3;
+var subtraction=num1-num2;
+document.write('the subtraction of'+" "+num1+" "+'and'+" "+num2 +" "+'is'+" "+subtraction +"<br/>"+"<br/>")
+var product =num1*num2;
+document.write("the product of"+" "+num1+" "+'and'+" "+num2+" "+'is'+" "+product +"<br/>"+"<br/>")
+var division=num1/num2;
+document.write('the division of'+" "+num1+" "+'and'+" "+num2+'is'+" "+division+"<br/>"+"<br/>")
+var modulas=num1%num2;
+document.write('the modulas of'+" "+num1+" "+'and'+" "+num2+" "+'is'+" "+modulas)
